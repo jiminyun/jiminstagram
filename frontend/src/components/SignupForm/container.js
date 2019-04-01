0fab2a84b4f1c434a2b7fb86151c4fbcb20e528e
jiminyun <jiminyun7@gmail.com>
2019-04-01 15:53:13 -0400

    added form validation

--- frontend/src/components/SignupForm/container.js
+++ frontend/src/components/SignupForm/container.js
@@ -1,54 +1,96 @@
-import React , { Component } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import SignupForm from "./presenter";
 
-class Container extends Component{
-    state = {
-        email: "",
-        name: "",
-        username: "",
-        password: ""
+class Container extends Component {
+  state = {
+    email: "",
+    name: "",
+    username: "",
+    password: "",
+    formErrors: { email: "", name: "", username: "", password: "" },
+    formValid: false,
+    emailValid: false,
+    passwordValid: false
+  };
+  static propTypes = {
+    facebookLogin: PropTypes.func.isRequired,
+    usernameLogin: PropTypes.func.isRequired
+  };
+  render() {
+    return (
+      <SignupForm
+        {...this.state}
+        handleInputChange={this._handleInputChange}
+        handleSubmit={this._handleSubmit}
+        handleFacebookLogin={this._handleFacebookLogin}
+        errorClass={this._errorClass}
+      />
+    );
+  }
+
+  _handleInputChange = event => {
+    const {
+      target: { value, name }
+    } = event;
+    this.setState(
+      {
+        [name]: value
+      },
+      () => {
+        this._validateField(name, value);
+      }
+    );
+    //console.log(this.state);
+  };
+
+  _handleSubmit = event => {
+    const { email, name, password, username } = this.state;
+    const { createAccount } = this.props;
+    event.preventDefault();
+    createAccount(username, password, email, name);
+  };
+
+  _handleFacebookLogin = response => {
+    console.log(response);
+  };
+
+  _validateField = (fieldName, value) => {
+    let fieldValidationErrors = this.state.formErrors;
+    let emailValid = this.state.emailValid;
+    let passwordValid = this.state.passwordValid;
+
+    switch (fieldName) {
+      case "email":
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.email = emailValid ? "" : " is invalid";
+        break;
+      case "password":
+        passwordValid = value.length >= 6;
+        fieldValidationErrors.password = passwordValid ? "" : " is too short";
+        break;
+      default:
+        break;
     }
-    static propTypes = {
-        facebookLogin: PropTypes.func.isRequired,
-        usernameLogin: PropTypes.func.isRequired
-    };
-    render(){
-        const { email, name, username, password } = this.state ;
-        return (
-            <SignupForm
-             emailValue={email}
-             nameValue={name}
-             usernameValue={username}
-             passwordValue={password}
-             handleInputChange={this._handleInputChange}
-             handleSubmit={this._handleSubmit}
-             handleFacebookLogin={this._handleFacebookLogin}
-            />
-        )
-    }
+    this.setState(
+      {
+        formErrors: fieldValidationErrors,
+        emailValid: emailValid,
+        passwordValid: passwordValid
+      },
+      this._validateForm
+    );
+  };
 
-    _handleInputChange = event => {
-        const { target: { value, name } } = event;
-        this.setState({
-          [name]: value
-        });
-        //console.log(this.state);
-      };
+  _validateForm() {
+    this.setState({
+      formValid: this.state.emailValid && this.state.passwordValid
+    });
+  }
 
-    _handleSubmit = event => {
-        const {email, name, password, username} = this.state;
-        const { createAccount } = this.props;
-        event.preventDefault();
-        createAccount(username, password, email, name);
-
-    };
-
-    _handleFacebookLogin = response => {
-        console.log(response);
-    };
-    
+  _errorClass = error => {
+    return error.length === 0 ? "" : "has-error";
+  };
 }
 
 export default Container;
-
--- frontend/src/components/SignupForm/presenter.js
+++ frontend/src/components/SignupForm/presenter.js
@@ -2,75 +2,85 @@ import React from "react";
 import "shared/formStyles.scss";
 import PropTypes from "prop-types";
 import FacebookLogin from "react-facebook-login";
+import { FormErrors } from "formErrors";
 
 const SignupForm = (props, context) => (
-    <div className="formComponent">
-        <h3 className="signupHeader">
-          Sign up to see photos and videos from your friends.
-        </h3>
-        <FacebookLogin
-          appId="317763908819874"
-          autoLoad={true}
-          fields="name,email,picture"
-          callback={props.handleFacebookLogin}
-          cssClass="facebookLink"
-          icon="fa-facebook-official"
-        >
-          Log in with Facebook
-       </FacebookLogin>
-         <span className="divider">or</span>
-          <form className="form" onSubmit={props.handleSubmit}>
-            <input 
-              type="email" 
-              placeholder="Email" 
-              className="textInput"
-              value={props.emailValue}
-              onChange={props.handleInputChange}
-              name="email" />
-            <input 
-              type="text" 
-              placeholder="Full Name" 
-              className="textInput" 
-              value={props.nameValue}
-              onChange={props.handleInputChange}
-              name="name" />
-            <input
-              type="username"
-              placeholder="Username"
-              className="textInput"
-              value={props.usernameValue}
-              onChange={props.handleInputChange}
-              name="username" 
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              className="textInput"
-              value={props.passwordValue}
-              onChange={props.handleInputChange}
-              name="password" 
-            />
-            <input type="submit" value="Sign up" className="button" />
-          </form>
-        <p className="terms">
-          By signing up, you agree to our <span>Terms & Privacy Policy</span>.
-        </p>
-     </div>   
-  
-  );
+  <div className="formComponent">
+    <h3 className="signupHeader">
+      Sign up to see photos and videos from your friends.
+    </h3>
+    <FacebookLogin
+      appId="317763908819874"
+      autoLoad={true}
+      fields="name,email,picture"
+      callback={props.handleFacebookLogin}
+      cssClass="facebookLink"
+      icon="fa-facebook-official"
+    >
+      Log in with Facebook
+    </FacebookLogin>
+    <span className="divider">or</span>
+    <form className="form" onSubmit={props.handleSubmit}>
+      <input
+        type="email"
+        placeholder="Email"
+        className={`textInput ${props.errorClass(props.formErrors.email)}`}
+        value={props.email}
+        onChange={props.handleInputChange}
+        name="email"
+      />
+      <input
+        type="text"
+        placeholder="Full Name"
+        className="textInput"
+        value={props.name}
+        onChange={props.handleInputChange}
+        name="name"
+      />
+      <input
+        type="username"
+        placeholder="Username"
+        className="textInput"
+        value={props.username}
+        onChange={props.handleInputChange}
+        name="username"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className={`textInput
+        ${props.errorClass(props.formErrors.password)}`}
+        value={props.password}
+        onChange={props.handleInputChange}
+        name="password"
+      />
+      <input
+        type="submit"
+        value="Sign up"
+        className="button"
+        disabled={!props.formValid}
+      />
+      <div className="panel panel-default">
+        <FormErrors formErrors={props.formErrors} />
+      </div>
+    </form>
+    <p className="terms">
+      By signing up, you agree to our <span>Terms & Privacy Policy</span>.
+    </p>
+  </div>
+);
 
-  SignupForm.propTypes = {
-    emailValue: PropTypes.string.isRequired,
-    nameValue: PropTypes.string.isRequired,
-    usernameValue: PropTypes.string.isRequired,
-    passwordValue: PropTypes.string.isRequired,
-    handleInputChange: PropTypes.func.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
-    handleFacebookLogin: PropTypes.func.isRequired
-  }
+SignupForm.propTypes = {
+  email: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleFacebookLogin: PropTypes.func.isRequired
+};
 
-  SignupForm.contextTypes = {
-    t: PropTypes.func.isRequired
-  }
-  export default SignupForm ;
-  
+SignupForm.contextTypes = {
+  t: PropTypes.func.isRequired
+};
+export default SignupForm;
